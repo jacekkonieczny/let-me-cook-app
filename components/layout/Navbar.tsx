828dfb45688ac31877d81c35c79151f6333c5c30
jacekkonieczny <jacek.konieczny9@gmail.com>
2025-11-15 02:03:47 +0100

    add hover animation to navbar links ✌️

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -7,12 +7,12 @@ const Navbar = () => {
             <div className="pl-6 pr-3 py-3 flex items-center justify-between text-neutral-100">
                 <Link className="text-3xl font-bold" href="/">let me cook</Link>
                 <div className="flex items-center gap-7 font-semibold">
-                    <Link href="/about-us">About us</Link>
-                    <Link href="/recipes">Recipes</Link>
-                    <Link href="/pricing">Pricing</Link>
+                    <Link className="hover-underline-animation" href="/about-us">About us</Link>
+                    <Link className="hover-underline-animation" href="/recipes">Recipes</Link>
+                    <Link className="hover-underline-animation" href="/pricing">Pricing</Link>
                 </div>
                 <div className="flex items-center gap-7 font-semibold">
-                    <Link href="/login">Log in</Link>
+                    <Link className="hover-underline-animation" href="/login">Log in</Link>
                     <Link className="bg-green-600 hover:bg-green-700 px-5 py-2.5 rounded-full" href="/register">Get started for free</Link>
                 </div>
             </div>
